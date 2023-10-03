d0d186bb845d9f4301b40fb21face219b12318d0
Sameer Shaik <87945863+sameershaik-sudo@users.noreply.github.com>
2023-10-03 23:41:33 +0530

    Update portscanner.py

--- portscanner.py
+++ portscanner.py
@@ -1,14 +1,10 @@
-
-
 import socket
 import termcolor
-
 
 def scan(target, ports):
 	print('\n' + ' Starting Scan For ' + str(target))
 	for port in range(1,ports):
 		scan_port(target,port)
-
 
 def scan_port(ipaddress, port):
 	try:
@@ -18,7 +14,6 @@ def scan_port(ipaddress, port):
 		sock.close()
 	except:
 		pass
-
 
 targets = input("[*] Enter Targets To Scan(split them by ,): ")
 ports = int(input("[*] Enter How Many Ports You Want To Scan: "))
